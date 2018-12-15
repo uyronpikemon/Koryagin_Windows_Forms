2a18acbc46c42dbb803f174be94a6d16143c970d
Юрий Краснов <uyronpikemon@outlook.com>
2018-12-15 23:37:57 +0300

    fix

--- Koryagin_Windows_Forms/Form1.cs
+++ Koryagin_Windows_Forms/Form1.cs
@@ -1,14 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
-using System.Diagnostics;
 
 namespace Koryagin_Windows_Forms
 {
